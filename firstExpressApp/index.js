0fd9c119d3c64f7c763069208469b44e3365fa4e
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2022-08-18 10:45:01 +0530

    added index.js file and created our first server

---
+++ firstExpressApp/index.js
@@ -0,0 +1,16 @@
+// importing express.
+
+const express = require("express");
+// defining express function to a variable.
+const app = express();
+// adding a port to run the server.
+const port = 3000;
+//Mounts the specified middleware function or functions at the specified path: the middleware function is executed when the base of the requested path matches path.
+// app.use()
+app.use(function handeler(){
+    console.log("reloded the page")
+})
+//Binds and listens for connections on the specified host and port. app.listen()
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
