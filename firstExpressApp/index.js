b42675d1697cba6f13f9168c9ffc3a3d1e3570e4
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2022-08-18 11:14:48 +0530

    learned about requests and responses

--- firstExpressApp/index.js
+++ firstExpressApp/index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 //Mounts the specified middleware function or functions at the specified path: the middleware function is executed when the base of the requested path matches path.
 // app.use()
-app.use(function handeler(req, res){
+app.use(function handeler(req, res){ // request and response parameters are automaticly created by express these are javascript objects those have useful methods to work with like send the response. That we use in the exapmple.
     console.log("reloded the")
     res.send("hi".toUpperCase())
 })
